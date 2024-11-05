c82d108e70fee3a9747090b1f638edf9037723a0
saaketh180903 <saaketh.koduri@gmail.com>
2024-11-05 22:54:01 +0530

    Initial commit

--- index.js
+++ index.js
@@ -49,13 +49,11 @@ app.use(
     httpOnly: true,
   })
 );
-app.use(
-  cors({
-    credentials: true,
-    origin: "https://blog-mern-frondend.netlify.app",
-    methods: "GET,POST,PUT,DELETE",
-  })
-);
+app.use(cors({
+  origin: "http://localhost:3000", // Update with your frontend origin
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
